2f94b1cbab1e22281ae58fb3d55262fb14dc7ec6
dragonLee <944106568@qq.com>
2022-02-14 17:18:16 +0800

    初始化主应用项目模板

---
+++ src/micro/apps.ts
@@ -0,0 +1,56 @@
+import config from "@/config";
+
+console.log(config);
+const {
+  REACT_MICRO_APP,
+  VUE_MICRO_APP,
+  ANGULAR_MICRO_APP,
+  STATIC_MICRO_APP,
+} = config;
+
+const getActiveRule = (hash:string) => (location:Location) => {
+  return location.hash.split("/")[1] == hash
+};
+
+const apps = [
+  /**
+   * name: 微应用名称 - 具有唯一性
+   * entry: 微应用入口 - 通过该地址加载微应用，这里我们使用 config 配置
+   * container: 微应用挂载节点 - 微应用加载完成后将挂载在该节点上
+   * activeRule: 微应用触发的路由规则 - 触发路由规则后将加载该微应用
+   */
+  // {
+  //   name: "ReactMicroApp",
+  //   entry: REACT_MICRO_APP,
+  //   container: "#frame",
+  //   activeRule: "/react",
+  // },
+  {
+    name: "VueMicroApp",
+    entry: VUE_MICRO_APP,
+    container: "#frame",
+    activeRule: "/vue",
+    // activeRule: getActiveRule('vue'),
+  },
+  // {
+  //   name: "publicopinionmonitoring",
+  //   entry: "http://localhost:5023/",
+  //   container: "#frame",
+  //   // activeRule: "/vue",
+  //   activeRule: getActiveRule('esp-module-publicopinionmonitoring'),
+  // },
+  // {
+  //   name: "AngularMicroApp",
+  //   entry: ANGULAR_MICRO_APP,
+  //   container: "#frame",
+  //   activeRule: "/angular",
+  // },
+  // {
+  //   name: "StaticMicroApp",
+  //   entry: STATIC_MICRO_APP,
+  //   container: "#frame",
+  //   activeRule: "/static",
+  // },
+];
+
+export default apps;
